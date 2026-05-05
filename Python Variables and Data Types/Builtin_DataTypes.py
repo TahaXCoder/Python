ef962bab22ab73697ca559ef7c1127f2a43de0d4
Muhammad Taha <mttahasaleem123@gmail.com>
2026-05-05 23:12:25 +0500

    Re-add project folder

---
+++ Python Variables and Data Types/Builtin_DataTypes.py
@@ -0,0 +1,75 @@
+# TextType:    str
+
+#NUmeric Types:  int,float,complex
+
+#Sequence Types: list,tuple,range
+
+#Mapping Type : Dict
+
+#Boolean Type : bool
+
+#Binary Types : bytes, bytearray,memoryview
+
+#None Type : None Type
+
+
+# Examples of Different data types
+
+x = ("Hello world")
+print(x)
+
+# printing the data type
+print (type(x))
+
+
+x = (55)
+print(x)
+
+# printing the data type
+print (type(x))
+
+
+x = (67.5)
+print(x)
+
+# printing the data type
+print (type(x))
+
+x = True
+print(x)
+
+# printing the data type
+print (type(x))
+
+
+x = 1j
+print(x)
+
+# printing the data type
+print (type(x))
+
+x = ["apple","Bannana","cherry"]
+print(x)
+
+# printing the data type
+print (type(x))
+
+x = range(6)
+print(x)
+
+# printing the data type
+print (type(x))
+
+
+
+x = {"name":"Taha","age":22}
+print(x)
+
+# printing the data type
+print (type(x))
+
+x = range(6)
+print(x)
+
+# printing the data type
+print (type(x))
---
+++ Python Variables and Data Types/Variables.py
@@ -0,0 +1,26 @@
+print("Hello world")
+
+name = "Taha"
+age = 22
+
+height = 6
+
+# Rules for variables 
+
+# Valid
+my_name = "Taha"
+age2 = 20
+_private = "hidden"
+
+#  Invalid
+# 2age = 20        # can't start with number
+# my-name = "x"   # no hyphens
+# class = "CS"     # can't use reserved keywords
+
+# Multiple Assignments
+a = b = c = 0  # Same value can be assign
+print(b)
+
+Name , age ,height = "Taha" , 22 , 6      # for assigning the dfferent values
+
+print(height)
